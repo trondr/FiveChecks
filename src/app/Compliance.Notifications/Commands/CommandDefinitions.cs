4697a6f83a8ff9463ddf4dccf86676aded1389e4
trondr <trond@outlook.com>
2020-03-21 02:59:33 +0100

    Implement ShowNotification command and configure NCmdLiner to open help in Notepad.

---
+++ src/app/Compliance.Notifications/Commands/CommandDefinitions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+using Compliance.Notifications.Common;
+using Compliance.Notifications.Helper;
+using Compliance.Notifications.ToastTemplates;
+using Microsoft.Toolkit.Uwp.Notifications;
+using NCmdLiner.Attributes;
+using DesktopNotificationManagerCompat = Compliance.Notifications.Helper.DesktopNotificationManagerCompat;
+
+namespace Compliance.Notifications.Commands
+{
+
+    public static class CommandDefinitions
+    {
+        [Command(Summary = "Show example notification.",Description = "Show example notification.")]
+        // ReSharper disable once UnusedMember.Global
+        public static async Task<int> ShowNotification(
+            [RequiredCommandParameter(Description = "Arguments",AlternativeName = "a", ExampleValue = new[]{"SomeArg1","SomeArg2"})]
+            string[] args)
+        {
+            Logging.GetLogger("ShowNotification").Info($"Compliance.Notifications {FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion}");
+            DesktopNotificationManagerCompat.RegisterAumidAndComServer<MyNotificationActivator>("github.com.trondr.Compliance.Notifications");
+            DesktopNotificationManagerCompat.RegisterActivator<MyNotificationActivator>();
+
+            if (args.Contains(DesktopNotificationManagerCompat.ToastActivatedLaunchArg))
+            {
+                // Our NotificationActivator code will run after this completes,
+                // and will show a window if necessary.
+            }
+            else
+            {
+                var title = "Disk space is low!";
+                var image = GetRandomPicture();
+                var content =
+                    "Disk space is critically low. This have consequence for system stability, program installations and update of Windows.";
+                var companyName = "My Company AS";
+                BindableString content2 = "Please cleanup your disk";
+                var action = "ms-settings:storagesense";
+                var toastContent =
+                    await ActionSnoozeDismissToastContent.CreateToastContent(title, image, companyName, content, content2,
+                        action).ConfigureAwait(true);
+                // Create the XML document (BE SURE TO REFERENCE WINDOWS.DATA.XML.DOM)
+                var doc = new XmlDocument();
+                var toastXmlContent = toastContent.GetContent();
+                Console.WriteLine(toastXmlContent);
+                doc.LoadXml(toastContent.GetContent());
+                // And create the toast notification
+                var toast = new ToastNotification(doc);
+                // And then show it
+                DesktopNotificationManagerCompat.CreateToastNotifier().Show(toast);
+                Logging.GetLogger("ShowNotification").Info("Sleeping 5 seconds");
+                await Task.Delay(5000).ConfigureAwait(true);
+                Logging.GetLogger("ShowNotification").Info("Finished ShowNotification");
+            }
+            return 0;
+        }
+
+        private static readonly Random Rnd = new Random();
+        private static string GetRandomPicture()
+        {
+            var imageNumber = Rnd.Next(1, 900);
+            var image = $"https://picsum.photos/364/202?image={imageNumber}";
+            return image;
+        }
+    }
+}
--- src/app/Compliance.Notifications/Program.cs
+++ src/app/Compliance.Notifications/Program.cs
@@ -1,85 +1,39 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Reflection;
-using Windows.Data.Xml.Dom;
-using Windows.UI.Notifications;
-using Compliance.Notifications.Helper;
-using Microsoft.Toolkit.Uwp.Notifications;
-using DesktopNotificationManagerCompat = Compliance.Notifications.Helper.DesktopNotificationManagerCompat;
-using System.Linq;
 using System.Threading.Tasks;
+using Compliance.Notifications.Commands;
 using Compliance.Notifications.Common;
-using Compliance.Notifications.ToastTemplates;
 using LanguageExt;
+using NCmdLiner;
+using ApplicationInfo = Compliance.Notifications.Common.ApplicationInfo;
 
 namespace Compliance.Notifications
 {
     class Program
     {
-        private static Try<Task<int>> Run(string[] args) => () =>
+        private static TryAsync<int> TryRun(string[] args) => () =>
         {
-            Logging.DefaultLogger.Info($"Start: {ApplicationInfo.ApplicationName}.{ApplicationInfo.ApplicationVersion}. Command line: {Environment.CommandLine}");
-            var returnValue = 0;
-            Logging.DefaultLogger.Info($"Stop: {ApplicationInfo.ApplicationName}.{ApplicationInfo.ApplicationVersion}. Return value: {returnValue}");
-            return ShowNotification(args);
+            IApplicationInfo applicationInfo = new NCmdLiner.ApplicationInfo()
+            {
+                Authors = "github.com/trondr",
+                Copyright = "Copyright © github/trondr 2020",
+                Description = "Show compliance notifications to the user."
+            };
+            var returnValue = NCmdLiner.CmdLinery.Run(typeof(CommandDefinitions), args, applicationInfo, new NotepadMessenger());
+            return returnValue;
         };
+
+        private static int ErrorHandler(Exception ex, int exitCode)
+        {
+            Logging.WriteErrorToEventLog($"ERROR: {ex}");
+            return exitCode;
+        }
 
         static async Task<int> Main(string[] args)
         {
-            return await Run(args)
-                .Match(Succ: i => i, Fail: exception =>
-                {
-                    Logging.WriteErrorToEventLog($"ERROR: {exception.ToString()}");
-                    return Task.FromResult(1);
-                });
-        }
-
-        private static async Task<int> ShowNotification(string[] args)
-        {
-            Console.WriteLine(
-                $"Compliance.Notifications {FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion}");
-            DesktopNotificationManagerCompat.RegisterAumidAndComServer<MyNotificationActivator>(
-                "github.com.trondr.Compliance.Notifications");
-            DesktopNotificationManagerCompat.RegisterActivator<MyNotificationActivator>();
-
-            if (args.Contains(DesktopNotificationManagerCompat.TOAST_ACTIVATED_LAUNCH_ARG))
-            {
-                // Our NotificationActivator code will run after this completes,
-                // and will show a window if necessary.
-            }
-            else
-            {
-                var title = "Disk space is low!";
-                var image = GetRandomPicture();
-                var content =
-                    "Disk space is critically low. This have consequence for system stability, program installations and update of Windows.";
-                var companyName = "My Company AS";
-                BindableString content2 = "Please cleanup your disk";
-                var action = "ms-settings:storagesense";
-                var toastContent =
-                    await ActionSnoozeDismissToastContent.CreateToastContent(title, image, companyName, content, content2,
-                        action);
-                // Create the XML document (BE SURE TO REFERENCE WINDOWS.DATA.XML.DOM)
-                var doc = new XmlDocument();
-                var toastXmlContent = toastContent.GetContent();
-                Console.WriteLine(toastXmlContent);
-                doc.LoadXml(toastContent.GetContent());
-                // And create the toast notification
-                var toast = new ToastNotification(doc);
-                // And then show it
-                DesktopNotificationManagerCompat.CreateToastNotifier().Show(toast);
-                System.Threading.Thread.Sleep(5000);
-            }
-
-            return 0;
-        }
-
-        static Random _random = new Random();
-        private static string GetRandomPicture()
-        {
-            var imageNumber = _random.Next(1, 900);
-            var image = $"https://picsum.photos/364/202?image={imageNumber}";
-            return image;
+            Logging.DefaultLogger.Info($"Start: {ApplicationInfo.ApplicationName}.{ApplicationInfo.ApplicationVersion}. Command line: {Environment.CommandLine}");
+            var exitCode = await TryRun(args).Match(Succ: i => i, Fail: exception => ErrorHandler(exception,1)).ConfigureAwait(false);
+            Logging.DefaultLogger.Info($"Stop: {ApplicationInfo.ApplicationName}.{ApplicationInfo.ApplicationVersion}. Exit code: {exitCode}");
+            return exitCode;
         }
     }
 }
