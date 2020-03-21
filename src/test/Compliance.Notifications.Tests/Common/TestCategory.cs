848119533e846af782289e042b745b42b50748fb
trondr <trond@outlook.com>
2020-03-21 19:31:58 +0100

    Prepare test categories.

---
+++ src/test/Compliance.Notifications.Tests/Common/TestCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace Compliance.Notifications.Common.Tests
+{
+    public static class TestCategory
+    {
+        public const string UnitTests = "UnitTests";
+        public const string IntegrationTests = "IntegrationTests";
+        public const string ManualTests = "ManualTests";
+    }
+}
