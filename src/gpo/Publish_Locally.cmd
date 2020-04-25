e3a4a67aff6cfe5995704ad0342d9c0a993266e0
trondr <trond@outlook.com>
2020-04-25 22:14:04 +0200

    Update admx file

---
+++ src/gpo/Publish_Locally.cmd
@@ -0,0 +1,3 @@
+copy ".\github.trondr.Compliance.Notifications.admx" "C:\Windows\PolicyDefinitions\github.trondr.Compliance.Notifications.admx"
+copy ".\en-US\github.trondr.Compliance.Notifications.adml" "C:\Windows\PolicyDefinitions\en-US\github.trondr.Compliance.Notifications.adml"
+start gpedit.msc
