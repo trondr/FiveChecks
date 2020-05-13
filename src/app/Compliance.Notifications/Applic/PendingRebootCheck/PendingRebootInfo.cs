942c7543e0fd54e2bb07a2260060784e63ac69f7
trondr <trondr@outlook.com>
2020-05-13 18:44:12 +0200

    Code cleanup

--- src/app/Compliance.Notifications/Applic/PendingRebootCheck/PendingRebootInfo.cs
+++ src/app/Compliance.Notifications/Applic/PendingRebootCheck/PendingRebootInfo.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
 using LanguageExt;
 
 namespace Compliance.Notifications.Applic.PendingRebootCheck
