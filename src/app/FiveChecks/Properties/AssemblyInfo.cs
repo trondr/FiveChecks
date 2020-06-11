634d19c3eacdc739da151ea9175779d580483466
trondr <trondr@outlook.com>
2020-06-11 15:17:22 +0200

    FiveChecks.1.0.20163.46

--- src/app/FiveChecks/Properties/AssemblyInfo.cs
+++ src/app/FiveChecks/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("FiveChecks")]
 [assembly: AssemblyCopyright("Copyright © github.trondr 2020")]
 [assembly: AssemblyCompany("FiveChecks")]
-[assembly: AssemblyVersion("1.0.20141.46")]
-[assembly: AssemblyFileVersion("1.0.20141.46")]
+[assembly: AssemblyVersion("1.0.20163.46")]
+[assembly: AssemblyFileVersion("1.0.20163.46")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8B2765D7-A756-45AA-B7BD-4FB98F9F51F4")]
 [assembly: InternalsVisibleTo("FiveChecks.Tests")]
@@ -23,8 +23,8 @@ namespace System {
         internal const System.String AssemblyProduct = "FiveChecks";
         internal const System.String AssemblyCopyright = "Copyright © github.trondr 2020";
         internal const System.String AssemblyCompany = "FiveChecks";
-        internal const System.String AssemblyVersion = "1.0.20141.46";
-        internal const System.String AssemblyFileVersion = "1.0.20141.46";
+        internal const System.String AssemblyVersion = "1.0.20163.46";
+        internal const System.String AssemblyFileVersion = "1.0.20163.46";
         internal const System.Boolean ComVisible = false;
         internal const System.String Guid = "8B2765D7-A756-45AA-B7BD-4FB98F9F51F4";
         internal const System.String InternalsVisibleTo = "FiveChecks.Tests";
